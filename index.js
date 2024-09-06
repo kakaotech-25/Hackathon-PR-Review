5bc64f15fd24dccde0923c54150fd4fe7dbb8c1d
jaejunday <wowns8114@gmail.com>
2024-09-06 18:44:38 +0900

    fix: 리뷰 포맷에 파일명 명시

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ async function postReviewComment(octokit, owner, repo, pullRequestNumber, file, 
     owner,
     repo,
     issue_number: pullRequestNumber,
-    body: reviewMessage,
+    body: `review file: ${file.filename}\n\n${reviewMessage}`,
   });
   core.info(`Comment posted for file: ${file.filename}`); // 디버깅 구문
 }
